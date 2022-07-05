6daf9dc986d95b35ab735e890734db536c476781
bishoy237 <63172210+bishoy237@users.noreply.github.com>
2022-07-05 15:38:16 +0200

    counter for basketball
    using js

---
+++ 3.js
@@ -0,0 +1,34 @@
+let count = 0
+let num = document.getElementById('box1')
+
+function add1() {
+    count = count + 1
+    num.textContent = count
+
+}
+function add2() {
+    count = count + 2
+    num.textContent = count
+
+}
+
+function add3() {
+    count = count + 3
+    num.textContent = count
+
+}
+let cal = 0
+let digit = document.getElementById('box2')
+function sum1() {
+    cal = cal + 1
+    digit.textContent = cal
+}
+function sum2() {
+    cal = cal + 2
+    digit.textContent = cal
+}
+
+function sum3() {
+    cal = cal + 3
+    digit.textContent = cal
+}
